9c4ccaf5291ff0d0b07648220efd44d88728125a
shenghuadun <shengguolin123@163.com>
2014-12-08 10:57:50 +0800

    修改http.get

--- dtssAnWeihai2/src/com/dtssAnWeihai/tools/HttpUtil.java
+++ dtssAnWeihai2/src/com/dtssAnWeihai/tools/HttpUtil.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -79,9 +81,8 @@ public class HttpUtil {
 	}
 
 	public static String httpGet(String url, Map<String, String> params) {
-		URL u = null;
-		HttpURLConnection con = null;
-		// 构建请求参数
+		String result = "";
+		
 		StringBuffer sb = new StringBuffer();
 		if (params != null) {
 			for (Entry<String, String> e : params.entrySet()) {
@@ -92,41 +93,49 @@ public class HttpUtil {
 			}
 			sb.substring(0, sb.length() - 1);
 		}
-		System.out.println("send_url:" + url);
-		System.out.println("send_data:" + sb.toString());
-		// 尝试发送请求
-		try {
-			u = new URL(url);
-			con = (HttpURLConnection) u.openConnection();
-			con.setRequestMethod("get");
-			con.setDoOutput(true);
-			con.setDoInput(true);
-			con.setUseCaches(false);
-			con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream(), "UTF-8");
-			osw.write(sb.toString());
-			osw.flush();
-			osw.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (con != null) {
-				// con.disconnect();// 关闭socket
-			}
-		}
-		// 读取返回内容
-		StringBuffer buffer = new StringBuffer();
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-			String temp;
-			while ((temp = br.readLine()) != null) {
-				buffer.append(temp);
-				buffer.append("\n");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return buffer.toString();
+		
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + sb.toString();
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+//            // 遍历所有的响应头字段
+//            for (String key : map.keySet()) {
+//                System.out.println(key + "--->" + map.get(key));
+//            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+		
 	}
 	
 	/**
